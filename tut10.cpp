02a28a445483f2003c7081d203a704e28f5dc52e
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2023-08-30 01:02:45 +0530

    tut10.cpp

--- tut10.cpp
+++ tut10.cpp
@@ -18,18 +18,18 @@ int main(){
    //  loop body (C++ code);
    // }
 
-   // for (int i = 0; i < 40; i++)
-   // {
-   //  /* code */
-   //  cout << i << endl;
-   // }
+   for (int i = 0; i < 40; i++)
+   {
+    /* code */
+    cout << i << endl;
+   }
 
    // Example of infinite for loop   
-   // for (int i = 0; 34 <= 40; i++) // if we leave condition blank or put a condition which is always true then it will create a infinte for loop
-   // {
-   //  /* code */
-   //  cout << i << endl;
-   // }
+   for (int i = 0; 34 <= 40; i++) // if we leave condition blank or put a condition which is always true then it will create a infinte for loop
+   {
+    /* code */
+    cout << i << endl;
+   }
 
    /*While loop in c++*/
 
@@ -40,20 +40,20 @@ int main(){
    // }
 
    // Printing 1 to 40 using while loop
-   // int i = 1;
-   // while (i <= 40)
-   // {
-   //  cout << i << endl;
-   //  i++;
-   // }
+   int i = 1;
+   while (i <= 40)
+   {
+    cout << i << endl;
+    i++;
+   }
    
    // Example of infinite while loop   
-   // int i = 1;
-   // while (true) 
-   // {
-   //  cout << i << endl;
-   //  i++;
-   // }
+   int i = 1;
+   while (true) 
+   {
+    cout << i << endl;
+    i++;
+   }
 
    /*do-While loop in c++*/
 
@@ -63,11 +63,11 @@ int main(){
    // } while (condition);
 
    // Printing 1 to 40 using do-while loop 
-   // int i = 1; 
-   // do { 
-   //  cout << i << endl; 
-   //  i++; 
-   // } while (i <= 40);
+   int i = 1; 
+   do { 
+    cout << i << endl; 
+    i++; 
+   } while (i <= 40);
 
    // printing table of n using do-while loop
    int i = 1, k, n;
