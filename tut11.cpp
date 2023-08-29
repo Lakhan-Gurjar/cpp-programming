c9561edbc05329971e951c467570cad30397c436
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2023-08-30 01:03:59 +0530

    tut11.cpp

--- tut11.cpp
+++ tut11.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     /* code */
-    //     if(i==2){
-    //         break; ---> exit the looop
-    //     }
-    //     cout<<i<<endl;
-    // }
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        if(i==2){
+            break; ---> exit the looop
+        }
+        cout<<i<<endl;
+    }
 
     for (int i = 0; i < 40; i++)
     {
