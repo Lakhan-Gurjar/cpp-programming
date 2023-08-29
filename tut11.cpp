e7321c11add0c474b7b4112007292627764917ad
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2023-08-30 01:00:49 +0530

    Add files via upload

---
+++ tut11.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     /* code */
+    //     if(i==2){
+    //         break; ---> exit the looop
+    //     }
+    //     cout<<i<<endl;
+    // }
+
+    for (int i = 0; i < 40; i++)
+    {
+        /* code */
+        if(i==2){
+            continue; // ---> skip current iteration
+        }
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
