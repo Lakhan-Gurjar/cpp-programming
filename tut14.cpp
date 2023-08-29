0138eea811bc14cdd38452f3623bc9e961a4926f
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2023-08-30 01:05:37 +0530

    tut14.cpp

--- tut14.cpp
+++ tut14.cpp
@@ -21,22 +21,22 @@ int main() {
     enum Meal{ breakfast, lunch, dinner};
     Meal m1 = breakfast;
     cout << m1;
-    // cout << breakfast;
-    // cout << lunch;
-    // cout << dinner;
-    // union money m1;
-    // m1.rice = 34;
-    // m1.car = 'c';
-    // cout << m1.car;
+    cout << breakfast;
+    cout << lunch;
+    cout << dinner;
+    union money m1;
+    m1.rice = 34;
+    m1.car = 'c';
+    cout << m1.car;
 
-    // ep harry;
-    // struct employee shubham;
-    // struct employee rohanDas;
-    // harry.eID = 1;
-    // harry.favChar = 'c';
-    // harry.salary = 120000000;
-    // cout << harry.eID << endl;
-    // cout << harry.favChar << endl;
-    // cout << harry.salary << endl;
+    ep harry;
+    struct employee shubham;
+    struct employee rohanDas;
+    harry.eID = 1;
+    harry.favChar = 'c';
+    harry.salary = 120000000;
+    cout << harry.eID << endl;
+    cout << harry.favChar << endl;
+    cout << harry.salary << endl;
     return 0;
 }
